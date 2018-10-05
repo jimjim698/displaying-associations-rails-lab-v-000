9a67729af8df425679ee69dbac37e099365d1d48
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-05 01:40:29 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   
   def with_artist
-    "#{self.artist.name} - #{self.title}"
+    "#{self.artist_name} - #{self.title}"
   end 
   
   def artist_name 
